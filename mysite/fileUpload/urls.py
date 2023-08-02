63f3f280fe00800b22cfe4694216d594afd36f9d
pyk93 <pyk93@hanmail.net>
2023-08-02 16:18:30 +0900

    프런트엔드 구현 반영

--- mysite/fileUpload/urls.py
+++ mysite/fileUpload/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 app_name = "fileUpload"
 
 urlpatterns = [
-    path("", views.DocumentCreateView.as_view(), name="index"),
+    path("", TemplateView.as_view(template_name='fileUpload/Main.html'), name="index"),
+    path("help", TemplateView.as_view(template_name='fileUpload/help.html'), name="help"),
+    
     path("upload", views.DocumentCreateView.as_view(), name="upload"),
     path("show/<int:fid>", views.DocumentShowView.as_view(), name="show"),
     path("list", views.DocumentListView.as_view(), name="list"),
